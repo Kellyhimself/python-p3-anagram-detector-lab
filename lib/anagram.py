6c04737874869a11e18e5df105a751a02b2a42cb
Kitui Kelly <kituikelly@gmail.com>
2024-05-23 21:56:39 +0300

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, possibleAnagrams):
+        letter_list = [letter for letter in self.word]
+        return [possible_anagram for possible_anagram in possibleAnagrams if sorted(letter_list) == sorted(possible_anagram)]
+
